d2ae9182c6ad55c3d6f6e9dcef22216df54ae971
Hrafn <94292884+hrafn-07@users.noreply.github.com>
2024-03-26 13:32:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt update
-apt install gpg wget
+apt install -y gpg wget
 mkdir -p /etc/apt/keyrings
 gpg --homedir /tmp --no-default-keyring --keyring /etc/apt/keyrings/scylladb.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys d0a112e067426ab2
 wget -O /etc/apt/sources.list.d/scylla.list http://downloads.scylladb.com/deb/debian/scylla-5.4.list
